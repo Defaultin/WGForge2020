b401114b72a957dd14b57dd01e70cc55a05146fe
Kirill <50778301+Defaultin@users.noreply.github.com>
2020-09-29 13:04:05 +0300

    Add files via upload

--- D.py
+++ D.py
@@ -1,17 +1,56 @@
-from math import factorial as f
+CASH = dict()
 
 
-nPr = lambda n, r: f(n) / f(n-r)
-nCr = lambda n, r: f(n) / f(r) / f(n-r)
-round6 = lambda n: str(n) + '0' * 5 if n.is_integer() else str(round(n, 6))
-compare = lambda lst: sum(1 for i, n in enumerate(lst) if i + 1 != n)
+def round6(n):
+    '''45.0 -> 45.000000'''
+    n = round(n, 6)
+    return str(n) + '0' * (6 - len(str(n).split('.')[-1][:6]))
 
 
-def expected(lst):
-    res = 0.0
-    for i in range(1, compare(lst)):
-        res += nCr(10, i + 1)
-    return round6(res)
+def get_expected(lst):
+    '''Math expected value'''
+    h = hash(tuple(lst))
+    if CASH.get(h):
+        return CASH[h]
+
+    total, permutation = 0, 0
+    lenght = len(lst)
+
+    for i in range(lenght - 1):
+        for j in range(i + 1, lenght):
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
+                total += reduce(lst)
+                permutation += 1
+                lst[i], lst[j] = lst[j], lst[i]
+
+    result = (total + 45.0) / permutation
+    CASH[h] = result
+    return result
+
+
+def reduce(lst):
+    '''Reduces the permutation list'''
+    left, right = 0, 0
+    lenght = len(lst)
+
+    for i in range(lenght):
+        if lst[i] != i:
+            left = i
+            break
+
+    for i in range(lenght - 1, left, -1):
+        if lst[i] != i:
+            right = i
+            break
+
+    size = right - left + 1
+    if size == lenght:
+        return get_expected(lst)
+    elif 1 < size < 10:
+        return get_expected([lst[left + i] - left for i in range(size)])
+    else:
+        return 0.0
 
 
 def main():
@@ -19,11 +58,11 @@ def main():
     t = int(input())
     
     for _ in range(t):
-        l = map(int, input().split())
-        output.append(expected(l))
+        l = list(map(lambda x: int(x) - 1, input().split()))
+        output.append(reduce(l))
         
     for out in output:
-        print(out)
+        print(round6(out))
 
 
 if __name__ == '__main__':
